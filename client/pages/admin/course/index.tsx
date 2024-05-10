673f3690a6d17da61ddd763f41496a3455614d7f
IT21276446 <it21276446@my.sliit.lk>
2024-05-10 20:46:42 +0530

    Add admin pages

---
+++ client/pages/admin/course/index.tsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from 'react';
+import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTour } from '@reactour/tour';
+import PageWrapper from '../../../layout/PageWrapper/PageWrapper';
+
+import ThemeContext from '../../../context/themeContext';
+import useDarkMode from '../../../hooks/useDarkMode';
+import { TABS, TTabs } from '../../../common/type/helper';
+import Page from '../../../layout/Page/Page';
+
+
+import router from 'next/router';
+
+const Index: NextPage = () => {
+
+  return (
+    <PageWrapper>
+    
+
+      <Page container='fluid'>
+        <div className='row'>
+        
+        </div>
+
+      </Page>
+
+    </PageWrapper>
+  )
+}
+
+export default Index
